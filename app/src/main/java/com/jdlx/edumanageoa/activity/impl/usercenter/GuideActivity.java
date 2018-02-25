7c443dae3215dba6286c044babb73350c1411d8b
fupengpeng <564055710@qq.com>
2018-02-25 09:15:34 +0800

    引导界面创建

---
+++ app/src/main/java/com/jdlx/edumanageoa/activity/impl/usercenter/GuideActivity.java
@@ -0,0 +1,21 @@
+package com.jdlx.edumanageoa.activity.impl.usercenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jdlx.edumanageoa.R;
+
+
+/**
+ * @description  引导界面
+ * @author  fupengpeng
+ * @date  2018/2/25 0025 9:13
+ */
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
